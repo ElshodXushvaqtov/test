17748700d9d7f33f0b7c3b8603ed63802ab1bd9f
ELSHODBEK <elshodxushvaqtov07@gmail.com>
2023-02-08 22:42:21 +0500

    Test

--- app/src/main/java/uz/itschool/test/MainActivity.kt
+++ app/src/main/java/uz/itschool/test/MainActivity.kt
@@ -11,26 +11,54 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 var questions = ArrayList<test>()
 var count = 0
+var bool=false
 var index = 0
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
-        questions.add(test("1-2", "-1", "2", "3", "4"))
-        questions.add(test("2+2", "1", "2", "3", "4"))
-        questions.add(test("23+3", "1", "26", "3", "4"))
-        questions.add(test("3+2", "1", "2", "3", "5"))
+        questions.add(test("1-2", "-1", "2", "3", "4","-1"))
+        questions.add(test("2+2", "1", "2", "3", "4","4"))
+        questions.add(test("23+3", "1", "26", "3", "4","26"))
+        questions.add(test("3+2", "1", "2", "3", "5","5"))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         savol(0)
         questionNumber(questions.size)
-        next.setOnClickListener {
-            if (index >= questions.size) {
-index=0
-            } else {
-                index++
-                savol(index)
-                variantlar.clearCheck()
+
+        variant1.setOnClickListener {
+            if(variant1.text== questions[index].javob && !bool){
+                count++
+                togri_soni.text= count.toString()
+                bool=true
+                togri.visibility=View.VISIBLE
             }
+        }
+        variant2.setOnClickListener {
+            if(variant2.text== questions[index].javob && !bool){
+                count++
+                togri_soni.text= count.toString()
+                bool=true
+                togri.visibility=View.VISIBLE
+            }
+        }
+        variant3.setOnClickListener {
+            if(variant3.text== questions[index].javob && !bool){
+                count++
+                togri_soni.text= count.toString()
+                bool=true
+                togri.visibility=View.VISIBLE
+            }
+        }
+        variant4.setOnClickListener {
+            if(variant4.text== questions[index].javob && !bool){
+                count++
+                togri_soni.text= count.toString()
+                bool=true
+                togri.visibility=View.VISIBLE
+            }
+        }
+        next.setOnClickListener {
+next_question()
         }
     }
 
@@ -53,10 +81,27 @@ index=0
         variant3.text = test.variant3
         variant4.text = test.variant4
     }
-
+fun next_question(){
+    if (index < questions.size-1) {
+        index++
+        savol(index)
+        variantlar.clearCheck()
+        bool=false
+        togri.visibility=View.INVISIBLE
+    } else {
+        index=0
+        savol(index)
+        variantlar.clearCheck()
+        bool=false
+        togri.visibility=View.INVISIBLE
+    }
+}
     override fun onClick(p0: View?) {
         val btn = findViewById<Button>(p0!!.id)
         index = btn.tag.toString().toInt() - 1
         savol(index)
+        bool=false
+        togri.visibility=View.INVISIBLE
+        variantlar.clearCheck()
     }
 }
